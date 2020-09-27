c0bb603e8c06be6746845fcc340df0c61f904b8d
James Hu <james.hu.ustc@hotmail.com>
2020-09-27 17:31:37 +1000

    fix path

--- src/generator.ts
+++ src/generator.ts
@@ -52,8 +52,8 @@ export class Generator {
 
         const codeDir = path.resolve(__dirname);
         return Promise.all([
-            fs.copy(path.join(codeDir, 'src', 'index.html'), path.join(dir, 'index.html')),
-            fs.copy(path.join(codeDir, 'img'), path.join(dir, 'img')),
+            fs.copy(path.join(codeDir, '..', 'src', 'index.html'), path.join(dir, 'index.html')),
+            fs.copy(path.join(codeDir, '..', 'img'), path.join(dir, 'img')),
             fs.writeFile(path.join(dir, 'nodes.js'), 'var nodesArray = ' + JSON.stringify(this.generateNodes(), null, 2)),
             fs.writeFile(path.join(dir, 'edges.js'), 'var edgesArray = ' + JSON.stringify(this.generateEdges(), null, 2)),
         ]);
