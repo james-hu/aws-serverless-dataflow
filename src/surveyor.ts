3ae99cf527678adadfe92f1cc09ebdd311e9c389
James Hu <james.hu.ustc@hotmail.com>
2022-06-05 23:03:27 +1000

    use latest aws-utils

--- src/surveyor.ts
+++ src/surveyor.ts
@@ -1,6 +1,6 @@
 import { DateTime } from 'luxon';
 import { APIGateway, CloudFormation, Lambda, S3, SNS, SQS } from 'aws-sdk/clients/all';
-import { AwsUtils, withRetry } from '@handy-common-utils/aws-utils';
+import { AwsUtils, promiseWithRetry } from '@handy-common-utils/aws-utils';
 import { PromiseUtils } from '@handy-common-utils/promise-utils';
 import { Context } from './context';
 import buildIncludeExcludeMatcher from './matcher';
@@ -42,14 +42,14 @@ export class Surveyor {
     const apig = new APIGateway(this.context.awsOptions);
 
     // Domain Names
-    const domainNameObjects = await AwsUtils.repeatFetchingItemsByPosition(
-      pagingParam => withRetry(() => apig.getDomainNames({ limit: 100, ...pagingParam }).promise()),
+    const domainNameObjects = await AwsUtils.fetchAllByPosition<APIGateway.DomainName>(
+      pagingParam => promiseWithRetry(() => apig.getDomainNames({ limit: 100, ...pagingParam })),
     );
     await PromiseUtils.inParallel(parallelism, domainNameObjects, async domainNameObj => {
       const domainName = domainNameObj.domainName!;
       if (this.shouldInclude(domainName)) {
-        const mappings = await AwsUtils.repeatFetchingItemsByPosition(
-          pagingParam => withRetry(() => apig.getBasePathMappings({ domainName, limit: 500, ...pagingParam }).promise()),
+        const mappings = await AwsUtils.fetchAllByPosition<APIGateway.BasePathMapping>(
+          pagingParam => promiseWithRetry(() => apig.getBasePathMappings({ domainName, limit: 500, ...pagingParam })),
         );
         inventory.apigDomainNamesByName.set(domainName, {
           ...domainNameObj,
@@ -64,14 +64,14 @@ export class Surveyor {
     this.context.info(`Surveyed ${inventory.apigDomainNamesByName.size}/${domainNameObjects.length} domains in API Gateway`);
 
     // REST APIs
-    const restApis = await AwsUtils.repeatFetchingItemsByPosition(
-      pagingParam => withRetry(() => apig.getRestApis({ limit: 100, ...pagingParam }).promise()),
+    const restApis = await AwsUtils.fetchAllByPosition<APIGateway.RestApi>(
+      pagingParam => promiseWithRetry(() => apig.getRestApis({ limit: 100, ...pagingParam })),
     );
     await PromiseUtils.inParallel(parallelism, restApis, async restApi => {
       const restApiId = restApi.id!;
 
-      const resources = await AwsUtils.repeatFetchingItemsByPosition(
-        pagingParam => withRetry(() => apig.getResources({ ...pagingParam, restApiId }).promise()),
+      const resources = await AwsUtils.fetchAllByPosition<APIGateway.Resource>(
+        pagingParam => promiseWithRetry(() => apig.getResources({ ...pagingParam, restApiId })),
       );
       const lambdaFunctionArns = new Set<string>(); // at resource level
       const detailedResources = new Array<APIGateway.Resource & {
@@ -81,7 +81,7 @@ export class Surveyor {
         const resourceDetails = { ...resource, integrations: new Array<APIGateway.Integration & { lambdaFunctionArn: string | null }>() };
         if (resource.resourceMethods) {
           for (const httpMethod of Object.keys(resource.resourceMethods)) {
-            const integration = await withRetry(() => apig.getIntegration({ restApiId, resourceId: resource.id!, httpMethod }).promise());
+            const integration = await promiseWithRetry(() => apig.getIntegration({ restApiId, resourceId: resource.id!, httpMethod }));
             const lambdaFunctionArn = this.retrieveLambdaFunctionArnFromApiGatewayIntegrationUri(integration.uri);
             const integrationDetails = { ...integration, lambdaFunctionArn };
             if (lambdaFunctionArn) {
@@ -110,11 +110,12 @@ export class Surveyor {
     const inventory = this.context.inventory;
     const sqs = new SQS(this.context.awsOptions);
 
-    const queueUrls = await AwsUtils.repeatFetchingItemsByNextToken<string>('QueueUrls',
-      pagingParam => withRetry(() => sqs.listQueues({ ...pagingParam }).promise()),
+    const queueUrls = await AwsUtils.fetchAllByNextToken<string>(
+      pagingParam => promiseWithRetry(() => sqs.listQueues({ ...pagingParam })),
+      'QueueUrls',
     );
     await PromiseUtils.inParallel(parallelism, queueUrls, async queueUrl => {
-      const queueAttributes = (await withRetry(() => sqs.getQueueAttributes({ QueueUrl: queueUrl, AttributeNames: ['All'] }).promise())).Attributes!;
+      const queueAttributes = (await promiseWithRetry(() => sqs.getQueueAttributes({ QueueUrl: queueUrl, AttributeNames: ['All'] }))).Attributes!;
       queueAttributes.QueueUrl = queueUrl;    // add this for convenience
       const queueDetails = { ...queueAttributes, subscriptions: [] } as any;
       const queueArn = queueAttributes.QueueArn;
@@ -132,12 +133,13 @@ export class Surveyor {
     const sns = new SNS(this.context.awsOptions);
 
     // topics
-    const topics = await AwsUtils.repeatFetchingItemsByNextToken<SNS.Topic>('Topics',
-      pagingParam => withRetry(() => sns.listTopics({ ...pagingParam }).promise()),
+    const topics = await AwsUtils.fetchAllByNextToken<SNS.Topic>(
+      pagingParam => promiseWithRetry(() => sns.listTopics({ ...pagingParam })),
+      'Topics',
     );
     const topicArns = topics.map(topic => topic.TopicArn!);
     await PromiseUtils.inParallel(parallelism, topicArns, async topicArn => {
-      const topicAttributes = (await withRetry(() => sns.getTopicAttributes({ TopicArn: topicArn }).promise())).Attributes!;
+      const topicAttributes = (await promiseWithRetry(() => sns.getTopicAttributes({ TopicArn: topicArn }))).Attributes!;
       const topicDetails = { ...topicAttributes, subscriptions: [] } as any;
       if (this.shouldInclude(topicArn)) {
         inventory.snsTopicsByArn.set(topicArn, topicDetails);
@@ -146,14 +148,15 @@ export class Surveyor {
     this.context.info(`Surveyed ${inventory.snsTopicsByArn.size}/${topics.length} topics in SNS`);
 
     // subscriptions
-    const subscriptions = await AwsUtils.repeatFetchingItemsByNextToken<SNS.Subscription>('Subscriptions',
-      pagingParam => withRetry(() => sns.listSubscriptions({ ...pagingParam }).promise()),
+    const subscriptions = await AwsUtils.fetchAllByNextToken<SNS.Subscription>(
+      pagingParam => promiseWithRetry(() => sns.listSubscriptions({ ...pagingParam })),
+      'Subscriptions',
     );
     await PromiseUtils.inParallel(parallelism, subscriptions, async subscription => {
       const subscriptionArn = subscription.SubscriptionArn!;
       if (this.shouldInclude(subscription.TopicArn)) {
         try {
-          const subscriptionAttributes = (await withRetry(() => sns.getSubscriptionAttributes({ SubscriptionArn: subscriptionArn }).promise())).Attributes!;
+          const subscriptionAttributes = (await promiseWithRetry(() => sns.getSubscriptionAttributes({ SubscriptionArn: subscriptionArn }))).Attributes!;
           const subscriptionDetails = { ...subscriptionAttributes, ...subscription as Required<typeof subscription> };
           inventory.snsSubscriptionsByArn.set(subscriptionArn, subscriptionDetails);
           inventory.snsTopicsByArn.get(subscription.TopicArn!)?.subscriptions.push(subscriptionDetails);
@@ -174,12 +177,12 @@ export class Surveyor {
     const inventory = this.context.inventory;
     const s3 = new S3(this.context.awsOptions);
 
-    const buckets = (await withRetry(() => s3.listBuckets().promise())).Buckets ?? [];
+    const buckets = (await promiseWithRetry(() => s3.listBuckets())).Buckets ?? [];
     await PromiseUtils.inParallel(parallelism, buckets, async bucket => {
       if (this.shouldInclude(bucket.Name)) {
         const bucketName = bucket.Name!;
         const bucketArn = `arn::s3:::${bucketName}`;  // this is not the real ARN but should work for our purpose
-        const notificationConfiguration = await withRetry(() => s3.getBucketNotificationConfiguration({ Bucket: bucketName }).promise());
+        const notificationConfiguration = await promiseWithRetry(() => s3.getBucketNotificationConfiguration({ Bucket: bucketName }));
         const notifyLambdaFunctionArns = new Set((notificationConfiguration.LambdaFunctionConfigurations ?? []).map(c => c.LambdaFunctionArn).filter(arn => inventory.lambdaFunctionsByArn.has(arn)));
         const notifySqsQueueArns = new Set((notificationConfiguration.QueueConfigurations ?? []).map(c => c.QueueArn).filter(arn => inventory.sqsQueuesByArn.has(arn)));
         const notifySnsTopicArns = new Set((notificationConfiguration.TopicConfigurations ?? []).map(c => c.TopicArn).filter(arn => inventory.snsTopicsByArn.has(arn)));
@@ -200,14 +203,16 @@ export class Surveyor {
     const parallelism = this.context.options.flags.parallelism;
     const inventory = this.context.inventory;
     const lambda = new Lambda(this.context.awsOptions);
-    const functionConfigurations = await AwsUtils.repeatFetchingItemsByMarker<Lambda.FunctionConfiguration>('Functions',
-      pagingParam => withRetry(() => lambda.listFunctions({ ...pagingParam }).promise()),
+    const functionConfigurations = await AwsUtils.fetchAllByMarker<Lambda.FunctionConfiguration>(
+      pagingParam => promiseWithRetry(() => lambda.listFunctions({ ...pagingParam })),
+      'Functions',
     );
     await PromiseUtils.inParallel(parallelism, functionConfigurations, async functionConfiguration => {
       const functionArn = functionConfiguration.FunctionArn!;
       if (this.shouldInclude(functionArn)) {
-        const eventSourceMappings = await AwsUtils.repeatFetchingItemsByMarker<Lambda.EventSourceMappingConfiguration>('EventSourceMappings',
-          pagingParam => withRetry(() => lambda.listEventSourceMappings({ ...pagingParam, FunctionName: functionArn }).promise()),
+        const eventSourceMappings = await AwsUtils.fetchAllByMarker<Lambda.EventSourceMappingConfiguration>(
+          pagingParam => promiseWithRetry(() => lambda.listEventSourceMappings({ ...pagingParam, FunctionName: functionArn })),
+          'EventSourceMappings',
         );
         const detailedEventSourceMappings = eventSourceMappings.map(mapping => {
           let snsTopic;
@@ -242,16 +247,18 @@ export class Surveyor {
   async surveyCloudFormation(): Promise<void> {
     const inventory = this.context.inventory;
     const cf = new CloudFormation({ ...this.context.awsOptions, maxRetries: 7, retryDelayOptions: { customBackoff: (i => [300, 600, 1000, 2000, 3000, 5000, 8000, -1][i]) } });
-    const stacks = await AwsUtils.repeatFetchingItemsByNextToken<CloudFormation.StackSummary>('StackSummaries',
-      pagingParam => withRetry(() => cf.listStacks({ ...pagingParam }).promise(), [8000, 15000, 20000], [429, 400]),
+    const stacks = await AwsUtils.fetchAllByNextToken<CloudFormation.StackSummary>(
+      pagingParam => promiseWithRetry(() => cf.listStacks({ ...pagingParam }), [8000, 15000, 20000], [429, 400]),
+      'StackSummaries',
     );
     for (let i = 0; i < stacks.length; i++) {
       const stack = stacks[i];
       if (this.shouldInclude(stack.StackId)) {
         let resources = new Array<CloudFormation.StackResourceSummary>();
         try {
-          resources = await AwsUtils.repeatFetchingItemsByNextToken<CloudFormation.StackResourceSummary>('StackResourceSummaries',
-            pagingParam => withRetry(() => cf.listStackResources({ ...pagingParam, StackName: stack.StackId! }).promise(), [8000, 15000, 20000], [429, 400]),
+          resources = await AwsUtils.fetchAllByNextToken<CloudFormation.StackResourceSummary>(
+            pagingParam => promiseWithRetry(() => cf.listStackResources({ ...pagingParam, StackName: stack.StackId! }), [8000, 15000, 20000], [429, 400]),
+            'StackResourceSummaries',
           );
         } catch (error: any) {
           if (error.statusCode === 400) {
